6a76c29d384833acca280a314883d6f95125a66a
Razvan26 <34929885+Razvan26@users.noreply.github.com>
2018-03-31 00:54:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import time
 import calendar
-from tkinter import *
 
-root = Tk()
-bt1 = Button(self, fg = "red", bg = "blue")
-root.mainloop()
 print("If you want show calendar press 1")
 cheese = int(input("1[calendar]"))
 
