44877ca3b4851051603993b29c2e02125a1e6a38
Razvan26 <34929885+Razvan26@users.noreply.github.com>
2018-04-01 13:13:00 +0300

    Update cal.py

--- cal.py
+++ cal.py
@@ -1,4 +1,33 @@
-import calendar
 import time
-calend = calendar.month(2018, 3)
-print(calend)
+import calendar
+
+month = int(input("Enter mouth : "))
+
+if month == 1:
+    mshow = calendar.month(2018, 1)
+elif month == 2:
+    mshow = calendar.month(2018, 2)
+elif month == 3:
+    mshow = calendar.month(2018, 3)
+elif month == 4:
+    mshow = calendar.month(2018, 4)
+elif month == 5:
+    mshow = calendar.month(2018, 5)
+elif month == 6:
+    mshow = calendar.month(2018, 6)
+elif month == 7:
+    mshow = calendar.month(2018, 7)
+elif month == 8:
+    mshow = calendar.month(2018, 8)
+elif month == 9:
+    mshow = calendar.month(2018, 9)
+elif month == 10:
+    mshow = calendar.month(2018, 10)
+elif month == 11:
+    mshow = calendar.month(2018, 11)
+elif month == 12:
+    mshow = calendar.month(2018, 12)
+else:
+    print("1 to 12")
+print(mshow)
+
